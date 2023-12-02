0369445db7eacabdd4aa06bf61728a00b38423af
Victor Piskunovich <125730030+VictorPiskunovich@users.noreply.github.com>
2023-12-02 20:41:58 +0600

    OOP_Lab_4
    OOP_Lab_4

---
+++ src/Main.java
@@ -0,0 +1,52 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Random;
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        final ArrayList<String> strings = new ArrayList<String>();
+        strings.add("Задать движение по экрану строк");
+        strings.add("одна за другой");
+        strings.add("из массива строк");
+        strings.add("направление движения по апплету");
+        strings.add("и значение каждой строки");
+        strings.add("выбирается случайным образом");
+
+
+        final JFrame frame = new JFrame("Движение по экрану строк");
+        frame.setPreferredSize(new Dimension(800, 500));
+        frame.setVisible(true);
+        frame.setLayout(null);
+
+        final JLabel lbl = new JLabel();
+        lbl.setLocation(-1, 0);
+        lbl.setSize(300, 20);
+        frame.add(lbl);
+
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.pack();
+
+
+        Timer timer = new Timer(50, new ActionListener() {
+            int speedX, speedY; // скорость движения текста
+            Random rnd = new Random();
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                Point loc = lbl.getLocation();
+
+               
+                if (loc.x > frame.getWidth() || loc.y > frame.getHeight() || loc.x < 0 || loc.y < 0) {
+                    lbl.setLocation(frame.getWidth()/2, frame.getHeight()/2); //  центр окна
+                    speedX = -5+rnd.nextInt(10); // задаю случайную скорость
+                    speedY = -5+rnd.nextInt(10); // задаю случайную скорость
+                    lbl.setText(strings.get(rnd.nextInt(strings.size()-1))); // задаю случайную строку
+                } else {
+                    lbl.setLocation(loc.x + speedX, loc.y + speedY); // свдигаю лейбл с заданной скоростью
+                }
+            }});
+        timer.start();
+    }
+}
